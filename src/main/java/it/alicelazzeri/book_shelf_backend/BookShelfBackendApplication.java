579a1ea56e8f03b292d2b88b7a5ced99833fdd8f
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-08-04 19:08:24 +0200

    Initial commit

---
+++ src/main/java/it/alicelazzeri/book_shelf_backend/BookShelfBackendApplication.java
@@ -0,0 +1,13 @@
+package it.alicelazzeri.book_shelf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShelfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShelfBackendApplication.class, args);
+	}
+
+}
