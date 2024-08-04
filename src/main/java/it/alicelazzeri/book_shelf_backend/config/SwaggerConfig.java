16766ef931f288dea8a00b4917748f312622fbf5
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-08-05 01:44:21 +0200

    added swagger configuration

---
+++ src/main/java/it/alicelazzeri/book_shelf_backend/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package it.alicelazzeri.book_shelf_backend.config;
+
+import com.cloudinary.Cloudinary;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("BookShelf")
+                        .description("REST APIs for BookShelf")
+                        .version("1.0")
+                        .contact(new Contact().name("Alice Lazzeri").email("alice.lazzeri@hotmail.it")
+                                .url("https://alicelazzeri.netlify.app/"))
+                        .license(new License().name("License of API").url("API license URL")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("bearer");
+    }
+}
